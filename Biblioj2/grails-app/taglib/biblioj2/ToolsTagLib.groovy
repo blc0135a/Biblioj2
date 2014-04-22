e0c230b89ceb966567136ac43997ea0d105ed7fc
blc0135a <charly.blanc@laposte.net>
2014-04-23 00:50:23 +0200

    Création du tagLib panier toujours visible

---
+++ Biblioj2/grails-app/taglib/biblioj2/ToolsTagLib.groovy
@@ -0,0 +1,28 @@
+package biblioj2
+
+class ToolsTagLib {
+
+	def panier = { attrs, body ->
+		
+		Reservation maReservation = (Reservation) attrs.maReservation
+		
+		out << '<ul class="perso_nav">'
+		out << '<li class="perso_nav-item">'
+		
+		if(maReservation == null || maReservation.livresReserves == null) {
+			out << "<a href='http://localhost:8080/BiblioJ/reservation/showCurrentReservation'> Panier <br/> Total : 0 livre</a>"
+		} else {
+			out << "<a class=\"perso_icone\" href=\"http://localhost:8080/BiblioJ/reservation/showCurrentReservation\"><img src=\"${resource(dir: 'images', file: 'cartBig.png')}\" alt=\"Panier\" height=\"37\"/><br/> Total : " + maReservation.livresReserves.size() + " livres</a>"
+			out << '<ul class="perso_nav perso_sub-nav">'
+		for(livre in maReservation.livresReserves) {
+			out << '<li class="perso_sub-nav-item"><a href="http://localhost:8080/BiblioJ/reservation/showCurrentReservation">' + livre.livre.titre + ' (x' + livre.quantite + ')</a></li>'
+		}
+			out << '</ul>'
+		}
+		
+			out << '</li>'
+			out << '</ul>'
+		
+		}
+	
+}
---
+++ Biblioj2/test/unit/biblioj2/ToolsTagLibTests.groovy
@@ -0,0 +1,17 @@
+package biblioj2
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(ToolsTagLib)
+class ToolsTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
